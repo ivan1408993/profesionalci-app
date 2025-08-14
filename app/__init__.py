5e76bad8043820ea91a5776700def2fb08710aba
ivan1408993 <lazici19@gmail.com>
2025-08-14 08:17:23 +0200

    test za apsolutnu putanju u init.py

--- app/__init__.py
+++ app/__init__.py
@@ -37,7 +37,9 @@ def create_app():
 
     # Babel podešavanja
     app.config['BABEL_DEFAULT_LOCALE'] = 'sr'
-    app.config['BABEL_TRANSLATION_DIRECTORIES'] = 'app/translations'
+    import os
+    app.config['BABEL_TRANSLATION_DIRECTORIES'] = os.path.join(os.path.dirname(__file__), 'translations')
+
 
     # Email podešavanja
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
