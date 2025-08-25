755f1397da4a5bbd9282ef44429064101d61d757
ivan1408993 <lazici19@gmail.com>
2025-08-25 22:29:50 +0200

    vracen status superadmina u add_admin.py - test 1ž

--- add_admin.py
+++ add_admin.py
@@ -5,9 +5,24 @@ app = create_app()
 
 with app.app_context():
     admin = Employer.query.filter_by(email="lazici19@gmail.com").first()
+  # ili izaberi konkretnog po emailu
     if admin:
-        admin.is_superadmin = False   # uklanja status superadmina
+        admin.is_superadmin = True
         db.session.commit()
-        print("🚫 Nalog više nije superadmin.")
+        print("✅ Nalog je postao superadmin.")
     else:
-        print("❌ Nema nijednog poslodavca u bazi sa tim mejlom.")
+        print("❌ Nema nijednog poslodavca u bazi.")
+
+#from app import create_app, db
+#from app.models import Employer
+
+#app = create_app()
+
+#with app.app_context():
+ #   admin = Employer.query.filter_by(email="lazici19@gmail.com").first()
+  #  if admin:
+   #     admin.is_superadmin = False   # uklanja status superadmina
+    #    db.session.commit()
+     #   print("🚫 Nalog više nije superadmin.")
+    #else:
+    #    print("❌ Nema nijednog poslodavca u bazi sa tim mejlom.")
